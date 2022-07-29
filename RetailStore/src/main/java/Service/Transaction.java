dfef53e22d4c3fdda146542d874a3142ea3773fc
ashik <ashik@DESKTOP-K7PHPH2>
2022-07-29 14:45:03 +0530

    added fun in service layer

--- RetailStore/src/main/java/Service/Transaction.java
+++ RetailStore/src/main/java/Service/Transaction.java
@@ -3,4 +3,5 @@ package Service;
 public interface Transaction {
    double generateBill(int transactionID, String customerName, int noofBooks, int noofCd, int noofCosmetics);
    double showBill(int transactionID);
+   void fun();
 }
