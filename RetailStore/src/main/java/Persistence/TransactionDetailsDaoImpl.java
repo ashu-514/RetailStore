8b45e5177fa6759085b3bd15a7197d80e817c0e7
Tejaswini <guttulatejaswini@gmail.com>
2022-07-29 13:05:28 +0530

    transaction_details show method

--- RetailStore/src/main/java/Persistence/TransactionDetailsDaoImpl.java
+++ RetailStore/src/main/java/Persistence/TransactionDetailsDaoImpl.java
@@ -1,10 +1,33 @@
 package Persistence;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 public class TransactionDetailsDaoImpl implements TransactionDetailsDao {
 
 	@Override
 	public void showalltransactionDetails() {
-		// TODO Auto-generated method stub
+		try( Connection connection=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/retailstore", "root", "wiley");
+				Statement statement=connection.createStatement();	) {
+
+			ResultSet resultSet= statement.executeQuery("SELECT * FROM retailstore");
+			
+			while(resultSet.next()) {
+				int Transaction_id=resultSet.getInt("transaction_id");
+				int Item_Id=resultSet.getInt("item_Id");
+				int quantity=resultSet.getInt("quantity");
+	
+				System.out.println(Transaction_id+" "+Item_Id+" "+quantity);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 
 	}
 
