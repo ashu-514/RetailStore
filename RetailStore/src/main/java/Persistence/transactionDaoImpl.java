a0eb954143ac0bb2a256253870d36c6c3a3e9dbc
Harmeet Kaur <hamu20017@gmail.com>
2022-07-29 13:41:22 +0530

    transactionDaoimpl updated

--- RetailStore/src/main/java/Persistence/transactionDaoImpl.java
+++ RetailStore/src/main/java/Persistence/transactionDaoImpl.java
@@ -4,7 +4,22 @@ public class transactionDaoImpl implements TransactionDao {
 
 	@Override
 	public void showalltransactions() {
-		// TODO Auto-generated method stub
+		try( Connection connection=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/?user=root", "root", "wiley");
+				Statement statement=connection.createStatement();	) {
+
+			ResultSet resultSet= statement.executeQuery("SELECT * FROM Transaction");
+			
+			while(resultSet.next()) {
+				int Transaction_ID=resultSet.getInt("Transaction_Id");
+				int Customer_ID=resultSet.getString("Customer_ID");
+
+				
+				System.out.println(Transaction_ID+" "+Customer_ID);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 	}
 
