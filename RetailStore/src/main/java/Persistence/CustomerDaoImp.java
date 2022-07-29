7d45e14e5412b28d516b2adf844698bf49f06b84
“sahilthool” <sahilthool997@gmail.com>
2022-07-29 13:02:34 +0530

    custumerdao updated

--- RetailStore/src/main/java/Persistence/CustomerDaoImp.java
+++ RetailStore/src/main/java/Persistence/CustomerDaoImp.java
@@ -3,13 +3,37 @@ package Persistence;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+
 
 import Bean.Customer;
 
 public class CustomerDaoImp implements CustomerDao {
 
-	
+	@Override
+	public void showCustomer() {
+		try( Connection connection=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/retailstore", "root", "root");
+				Statement statement=connection.createStatement();	) {
+
+			ResultSet resultSet= statement.executeQuery("SELECT * FROM retailstore");
+			
+			while(resultSet.next()) {
+				int customer_id=resultSet.getInt("customer_Id");
+				String customer_name=resultSet.getString("customer_Name");
+				String user_name=resultSet.getString("user_name");
+				String password=resultSet.getString("passwords");
+								
+				System.out.println(customer_id +" "+customer_name+" "+user_name+" "+password);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
 
 	@Override
 	public void deleteCustomer() {
@@ -31,10 +55,6 @@ public class CustomerDaoImp implements CustomerDao {
 	}
 
 
-	@Override
-	public void showCustomer() {
-		// TODO Auto-generated method stub
-		
-	}
+	
 
 }
